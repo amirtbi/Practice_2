f373899771ec813a910e32d6191cfb665499b189
InitialCommit <torabi46@gmail.com>
2018-04-04 18:48:36 +0430

    Add project files.

---
+++ Practice_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            Console.Write("Enter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your lastname: ");
+            string lastname = Console.ReadLine();
+            string Full_name = myProgram.fullname(name, lastname);
+            Console.WriteLine(Full_name);
+
+
+
+            Console.ReadKey();
+
+        }
+
+       // Function:
+        string fullname(string name, string lastname)
+        {
+            return "The fullname is " + name + " " + lastname;
+        }
+
+    }
+}
